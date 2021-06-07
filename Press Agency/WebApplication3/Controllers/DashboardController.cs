1ac12740ce8c128ec6ac91fc8dd85c692f7a882f
Alaa2518 <alaazaher02@gmail.com>
2021-06-07 03:27:17 +0200

    Add edit post and delete

--- Press Agency/WebApplication3/Controllers/DashboardController.cs
+++ Press Agency/WebApplication3/Controllers/DashboardController.cs
@@ -91,6 +91,108 @@ namespace WebApplication3.Controllers
             return View(person);
         }
 
+        public ActionResult PostsPage()
+        {
+            Database.SetInitializer<ProductMangerContext>(null);
+
+            PostesImagesView postesImages = new PostesImagesView();
+            postesImages.articles = db.articles.ToList().Where(x => x.IfAproveed == true);
+            postesImages.photos = db.photos.ToList();
+            return View(postesImages);
+        }
+
+        public ActionResult PostsRequests()
+        {
+            Database.SetInitializer<ProductMangerContext>(null);
+
+            PostesImagesView postesImages = new PostesImagesView();
+            postesImages.articles = db.articles.ToList().Where(x => x.IfAproveed == false);
+            postesImages.photos = db.photos.ToList();
+            return View(postesImages);
+        }
+
+        public ActionResult DeletePost(int id)
+        {
+            Article article = db.articles.SingleOrDefault(x => x.Id == id);
+            db.articles.Remove(article);
+            db.SaveChanges();
+            return RedirectToAction("PostsPage");
+
+        }
+
+        public ActionResult Approve(int id)
+        {
+            Article art = db.articles.SingleOrDefault(x => x.Id == id);
+            Article article = new Article();
+            article = art;
+            article.IfAproveed = true;
+            db.Entry(article).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("PostsRequests");
+
+        }
+
+        public ActionResult EditPost(int id)
+        {
+
+            
+            NewPostsView postesImages = new NewPostsView();
+            var articles = db.articles.Single(x => x.Id == id);
+            var photos = db.photos.Single(x =>x.Aritcle_Id == id);
+            postesImages.ArticleBody = articles.ArticleBody;
+            postesImages.ArticleTitle = articles.ArticleTitle;
+            postesImages.ArticleType = articles.ArticleType;
+            postesImages.Id = articles.Id;
+            postesImages.image = photos.Path;
+                
+                
+             return View(postesImages);
+        }
+
+        [HttpPost]
+        public ActionResult EditPost(NewPostsView post, HttpPostedFileBase file)
+        {
+            Article articles = new Article();
+            Photo Images = new Photo();
+            if (ModelState.IsValid)
+            {
+                if (file != null)
+                {
+                    string pic = System.IO.Path.GetFileName(file.FileName);
+                    string path = System.IO.Path.Combine(Server.MapPath("~/images/profiles/"), pic);
+
+                    file.SaveAs(path);
+                    post.image = pic;
+
+                }
+                var article = db.articles.Single(x => x.Id == post.Id);
+                Photo Image = db.photos.SingleOrDefault(x => x.Aritcle_Id == post.Id);
+                articles = article;
+                articles.ArticleBody = post.ArticleBody;
+                articles.ArticleTitle = post.ArticleTitle; 
+                articles.ArticleType =  post.ArticleType; 
+                if (Image == null) {
+
+                    Images.Aritcle_Id = post.Id;
+                    Images.Path = post.image;
+                    Images.Title = post.ArticleTitle;
+                    db.photos.Add(Images);
+                    db.SaveChanges();
+                    db.Entry(Images).State = EntityState.Modified;
+                } else
+                {
+                    Images = Image;
+                    Images.Path = post.image;
+                    db.Entry(articles).State = EntityState.Modified;
+                    db.Entry(Images).State = EntityState.Modified;
+                }
+               
+                db.SaveChanges();
+                return RedirectToAction("PostsPage");
+            }
+            return View(post);
+        }
+       
 
     }
 }
